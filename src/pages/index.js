3777046c7da688bc681e4e9fd729efb5fe1a3258
Patrick Wong <saihowong81@hotmail.com>
2022-10-26 09:15:03 +0100

    Course project setup, hot reloading fixed, navbar/ sidebar/ aboutpage and a few other small components implemented

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import AboutPage from './AboutPage';
+import CartPage from './CartPage';
+import CheckoutPage from './CheckoutPage';
+import ErrorPage from './ErrorPage';
+import HomePage from './HomePage';
+import PrivateRoute from './PrivateRoute';
+import ProductsPage from './ProductsPage';
+import SingleProductPage from './SingleProductPage';
+
+export {
+  AboutPage,
+  CartPage,
+  CheckoutPage,
+  ErrorPage,
+  HomePage,
+  PrivateRoute,
+  ProductsPage,
+  SingleProductPage,
+};
